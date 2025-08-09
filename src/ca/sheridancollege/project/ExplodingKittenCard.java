ded6c974dd062481047bc8320af56ef202eeca7a
ten <116782797+dddtenhp@users.noreply.github.com>
2025-08-09 05:35:07 -0400

    Delete src/ca/sheridancollege/project/ExplodingKittenCard.java

--- src/ca/sheridancollege/project/ExplodingKittenCard.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.project;
-
-public class ExplodingKittenCard extends Card {
-
-    @Override
-    public String toString() {
-        return "Exploding Kitten - Boom! You're out unless you defuse.";
-    }
-}
