fc660046b3b433e1a5fd418609cbb6d5d9d4c1ab
ten <116782797+dddtenhp@users.noreply.github.com>
2025-08-09 20:06:07 -0400

    Add files via upload

---
+++ src/ca/sheridancollege/project/CardType.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.project;
+
+public enum CardType {
+    EXPLODING_KITTEN,
+    DEFUSE,
+    ATTACK,
+    SKIP,
+    NOPE,
+    FAVOR,
+    SHUFFLE,
+    SEE_THE_FUTURE,
+    CAT        
+}
